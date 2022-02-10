640e761ff1199fac5ba0e5386e6349b2132c035e
PGW <lomi1241@gmail.com>
2022-02-10 23:10:42 +0900

    ESLint, Prettier 설정 추가

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    endOfLine: "lf",
+    tabWidth: 4,
+    useTabs: true,
+    semi: true,
+    singleQuote: false,
+    trailingComma: "all",
+    printWidth: 90,
+    htmlWhitespaceSensitivity: "ignore",
+};
--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 
 export default function App() {
-    return (
-        <div>
-            <p>Hello React</p>
-        </div>
-    );
-};
+	return (
+		<div>
+			<p>Hello React</p>
+		</div>
+	);
+}
