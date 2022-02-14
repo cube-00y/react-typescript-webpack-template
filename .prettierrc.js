ea99b11e815827acd4364d113dd3d29eeae697d4
PGW <lomi1241@gmail.com>
2022-02-14 22:59:37 +0900

    feat: Emotion, reset style 추가

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,10 +1,9 @@
 module.exports = {
-    endOfLine: "lf",
-    tabWidth: 4,
-    useTabs: true,
-    semi: true,
-    singleQuote: false,
-    trailingComma: "all",
-    printWidth: 90,
-    htmlWhitespaceSensitivity: "ignore",
+	tabWidth: 4,
+	useTabs: true,
+	semi: true,
+	singleQuote: false,
+	trailingComma: "all",
+	printWidth: 90,
+	htmlWhitespaceSensitivity: "ignore",
 };
--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+import { Global } from "@emotion/react";
 import React from "react";
+import reset from "@/styles/reset";
 
 export default function App() {
 	return (
 		<div>
+			<Global styles={reset} />
 			<p>Hello React</p>
 		</div>
 	);
